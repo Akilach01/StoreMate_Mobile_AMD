30f97ac38ac7f59818da2857033fd9ac7bb8c635
Akilach01 <akilapeiris92@gmail.com>
2026-01-23 14:02:50 +0530

    setting up

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-blue-600">
+        StoreMate App 
+      </Text>
+    </View>
+  );
+}
