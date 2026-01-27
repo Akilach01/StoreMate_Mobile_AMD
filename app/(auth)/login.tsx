2ad027d798b43dad82cbe58e7e7f6bb4e68fb381
Akilach01 <akilapeiris92@gmail.com>
2026-01-27 22:35:43 +0530

    implementing login page

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "expo-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+
+
+export default function Login(){
+    const router = useRouter();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] =useState(false);
+
+    const handleLogin = async()=>{
+        if (!email || !password) {
+            Alert.alert("Error", "please fill all fields");
+            return;
+        }
+
+        try {
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password );
+            router.replace("/(dashboard)/home");
+        } catch (error:any){
+            Alert.alert("Login failed", error.message);
+        }finally{
+            setLoading(false);
+        }
+            
+        };
+
+        return(
+
+        )
+    }
