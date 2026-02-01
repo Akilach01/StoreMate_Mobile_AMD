d050ac9a3b98bf0fccdcf34e2cf988b9af20f5b4
Akilach01 <akilapeiris92@gmail.com>
2026-02-01 11:40:15 +0530

    fixing app issues

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,12 +1,13 @@
-import { Touchable, TouchableOpacity, View } from "react-native";
-
+import {  TouchableOpacity, View, Text } from "react-native";
+import { auth } from "../../services/firebase";
+import { signOut } from "firebase/auth";
 
 export default function Home(){
     return(
         <View className="flex-1 items-center justify-center">
             <Text className="text-lg">Welcome to StoreMate</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity
                 onPress={() =>signOut(auth)}
                 className="mt-4 px-4 py-2 bg-red-500 rounded">
                 <Text className="text-white text-center">Sign Out</Text>
