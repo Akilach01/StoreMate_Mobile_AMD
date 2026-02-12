7c487868da2b89f023d475efa4cac4d6340d26f2
Akilach01 <akilapeiris92@gmail.com>
2026-02-12 22:48:27 +0530

    fixing loading issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { Slot } from "expo-router";
-
+import { StatusBar, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout(){
     return(
-        <AuthProvider>
-            <Slot />
-        </AuthProvider>
+        <SafeAreaProvider>
+            <AuthProvider>
+                <View style={{flex: 1, backgroundColor: '#ffffff'}}>
+                    <StatusBar barStyle="dark-content" />
+                    <Slot />
+                </View>
+            </AuthProvider>
+        </SafeAreaProvider>
     );
 }
